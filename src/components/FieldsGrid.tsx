ad93ad241090ca830f5dd84da44b17813db68f93
Alexandru MOLODOI <alexandrumm@hotmail.com>
2017-03-31 00:06:57 -0700

    fixed grid

--- src/components/FieldsGrid.tsx
+++ src/components/FieldsGrid.tsx
@@ -8,25 +8,31 @@ export class FieldsGrid extends React.Component<IPropsFieldsGrid, any> {
     }
     render() {
         debugger;
-        const el = this.props.fields
-            ? this.props.fields.items.map((field) =>
-                <FieldRow Required={field.Required} Title={field.Title} TypeAsString={field.TypeAsString} Length={field.Length} />
-            )
-            : <tr></tr>;
+        let el: any;
+        if (this.props.fields) {
+            el =
+                <div>
+                    <table>
+                        <thead>
+                            <td>Title</td>
+                            <td>Length</td>
+                            <td>TypeAsString</td>
+                            <td>Required</td>
+                        </thead>
+                        <tbody>
+                            {this.props.fields.items.map((field) =>
+                                <FieldRow Required={field.Required} Title={field.Title} TypeAsString={field.TypeAsString} Length={field.Length} key={field.Title} />
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+        }
+        else {
+            el = <div></div>;
+        }
 
-        return (
-            <table>
-                <thead>
-                    <td>Title</td>
-                    <td>Length</td>
-                    <td>TypeAsString</td>
-                    <td>Required</td>
-                </thead>
-                <tbody>
-                    {el}
-                </tbody>
-            </table>
-        );
+        return el;
+
     }
 }
 
