fdb8e3807b8d1aa01514e40699c5a34e1d70f6d7
Alexandru MOLODOI <alexandrumm@hotmail.com>
2017-03-30 21:17:37 -0700

    added FieldsGrid component

--- src/components/FieldRow.tsx
+++ src/components/FieldRow.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
+import { SPFieldDefinition } from '../modules/DataImportModule';
 
-export class FieldRow extends React.Component<any, any> {
-    constructor(props) {
+export class FieldRow extends React.Component<SPFieldDefinition, any> {
+    constructor(props: SPFieldDefinition) {
         super(props);
     }
     render() {
         return (
             <tr>
-                <td></td>
+                <td>{this.props.ID}</td>
+                <td>{this.props.Title}</td>
+                <td>{this.props.TypeAsString}</td>                
             </tr>
         );
     }
 }
 
-export interface IFieldRowProps {
-    internalName: string,
-    fieldTypeAsString: string
-}
+// export interface IFieldRowProps {
+//     internalName: string,
+//     fieldTypeAsString: string,
+//     required: boolean,
+
+// }
