ff177d8aa0ff8498a5c8b34ec51757a60dbb3330
Alexandru MOLODOI <alexandrumm@hotmail.com>
2017-03-30 20:35:04 -0700

    working on FieldRow component

---
+++ src/components/FieldRow.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export class FieldRow extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <tr>
+                <td></td>
+            </tr>
+        );
+    }
+}
+
+export interface IFieldRowProps {
+
+}
